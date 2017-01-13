7da932746f2f6db25ec54aee255e4400b1228cb0
Lynn Xu <LynnHui819@gmail.com>
2017-01-13 14:10:20 -0800

    Initial Commit

---
+++ SecuritySDK/SecuritySDK.h
@@ -0,0 +1,19 @@
+//
+//  SecuritySDK.h
+//  SecuritySDK
+//
+//  Created by 李玲 on 1/13/17.
+//  Copyright © 2017 Jay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecuritySDK.
+FOUNDATION_EXPORT double SecuritySDKVersionNumber;
+
+//! Project version string for SecuritySDK.
+FOUNDATION_EXPORT const unsigned char SecuritySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecuritySDK/PublicHeader.h>
+
+
